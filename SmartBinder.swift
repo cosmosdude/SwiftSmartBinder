76834d516d8375326f194a6a9c3a63e39a077019
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2018-09-06 14:43:40 +0630

    Update SmartBinder.swift

--- SmartBinder.swift
+++ SmartBinder.swift
@@ -1,3 +1,11 @@
 infix operator <-
-func <- <T>(_ lhs: inout T, _ rhs : Any) { lhs = rhs as! T }
-func <- <T>(_ lhs: inout T, _ rhs : Any?) { lhs <- rhs! }
+
+func <- < T >(_ lhs: inout T, _ rhs: Any) {
+    lhs = rhs as! T
+}
+
+func <- < T > (_ lhs: inout T, _ rhs: Any?) {
+    if let value = rhs as? T {
+        lhs = value;
+    }
+}
